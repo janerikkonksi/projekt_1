7969d9897fe0ed60bdb780a38d8f31ab4485f58a
janerikkonksi <jan.konksi@mail.ee>
2019-03-21 10:29:01 +0200

    pane hullu

--- peaklass_uus.java
+++ peaklass_uus.java
@@ -5,7 +5,7 @@ public class peaklass_uus {
     public static void main(String[] args) {
 
         System.out.println("jou ");
-        // no tra mis pask see on
+        // see on uus messenger ju
         System.out.println("1. loo klient"); //eraklient, äriklient?
         System.out.println("2. loo konto"); //tavaKaart, kuldKaart
         System.out.println("3. näita kliendiga seotud kaarte");
